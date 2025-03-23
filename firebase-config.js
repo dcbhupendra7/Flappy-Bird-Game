8932ee2432f1e6850585a270dfcfd90df31f0508
dcbhupendra7 <dcbhupendra7@users.noreply.github.com>
2025-03-23 01:38:29 +0000

    deploy: aba00bfc4cea34f0d210834379d5c83ddd08c019

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAyDe6VdP_upXgI96xbHb4OE69-HzZ29Oo",
-  authDomain: "flappy-bird-game-ea2ed.firebaseapp.com",
-  projectId: "flappy-bird-game-ea2ed",
-  storageBucket: "flappy-bird-game-ea2ed.firebasestorage.app",
-  messagingSenderId: "817439627642",
-  appId: "1:817439627642:web:7787170407772ac3b6dec3"
-};
