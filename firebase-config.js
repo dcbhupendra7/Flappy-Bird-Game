29fc9c63633d3d500725b32730c07d6cd4d7e7e4
dcbhupendra7 <dcbhupendra7@users.noreply.github.com>
2025-03-23 02:06:54 +0000

    deploy: 0fa87219a2c33e04b5bf2bfd85c1a627f5a2b60d

--- firebase-config.js
+++ firebase-config.js
@@ -4,5 +4,7 @@ const firebaseConfig = {
   projectId: "flappy-bird-game-ea2ed",
   storageBucket: "flappy-bird-game-ea2ed.firebasestorage.app",
   messagingSenderId: "817439627642",
-  appId: "1:817439627642:web:7787170407772ac3b6dec3"
+  appId: "1:817439627642:web:7787170407772ac3b6dec3",
 };
+// Make firebaseConfig globally accessible
+window.firebaseConfig = firebaseConfig;
